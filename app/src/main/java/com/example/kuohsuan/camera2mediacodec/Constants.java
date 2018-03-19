993cea818f762d0a48d89f6717e3aea83dda41ef
peter <peter.tseng@geovision.com.tw>
2018-03-19 16:08:12 +0800

    First commit

---
+++ app/src/main/java/com/example/kuohsuan/camera2mediacodec/Constants.java
@@ -0,0 +1,8 @@
+package com.example.kuohsuan.camera2mediacodec;
+
+
+public class Constants {
+
+    final public static String SDACRD_DIR_APP_ROOT = "MediaCodecCamera2";
+    final public static String SDACRD_DIR_DB_BACKUP_DIR = SDACRD_DIR_APP_ROOT +"db_backup/";
+}
---
+++ app/src/main/java/com/example/kuohsuan/camera2mediacodec/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kuohsuan.camera2mediacodec;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i =new Intent();
+        i.setClass(this,SurfaceTextureCamera2Activity.class);
+        startActivity(i);
+    }
+}
