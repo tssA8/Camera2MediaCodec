120909d2eb83ea7a9489a7eb50e13cd472b99712
peter <peter.tseng@geovision.com.tw>
2018-04-03 17:38:24 +0800

    test

--- app/src/main/java/com/example/kuohsuan/camera2mediacodec/MainActivity.java
+++ app/src/main/java/com/example/kuohsuan/camera2mediacodec/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Intent i =new Intent();
         i.setClass(this,SurfaceTextureCamera2Activity.class);
         startActivity(i);
